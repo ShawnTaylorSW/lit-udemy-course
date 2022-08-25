d4f09ba8a57c9077cbbfd944317b9f507b64511b
ShawnTaylorSW <staylor@sandlotwizards.com>
2022-08-25 09:33:11 -0400

    3 files modified

---
+++ index.js
@@ -0,0 +1,17 @@
+import {LitElement, html} from '@polymer/lit-element';
+import '@polymer/paper-checkbox/paper-checkbox.js'
+
+class MyElement extends LitElement {
+
+  static get properties() {
+    return {
+      feeling: String
+    }
+  }
+  _render({feeling}) {
+    return html`<h1>Hello World! I am feeling ${feeling}</h1>
+      <paper-checkbox>Ready to deploy</paper-checkbox>`;
+  }
+}
+
+customElements.define('my-element', MyElement);
